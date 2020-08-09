d69c5038782f17c357eb7d0f5c390e29da90ef13
fsiddh <jasonshaw546@gmail.com>
2020-08-09 17:37:51 +0530

    updated views for more models and serializers

--- api_example/languages/views.py
+++ api_example/languages/views.py
@@ -2,9 +2,19 @@ from django.shortcuts import render
 
 from rest_framework import viewsets
 
-from .models import Language
-from .serializers import LanguageSerializer
+from .models import Language, Paradigm, Programmer
+from .serializers import LanguageSerializer, ParadigmSerializer, ProgrammerSerializer
+
+class ParadigmView(viewsets.ModelViewSet):
+    queryset = Language.objects.all()
+    serializer_class = ParadigmSerializer
+
 
 class LanguageView(viewsets.ModelViewSet):
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
+
+
+class ProgrammerView(viewsets.ModelViewSet):
+    queryset = Language.objects.all()
+    serializer_class = ProgrammerSerializer
