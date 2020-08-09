1a1594e412c84ae010372c1124be3facc7b7a67c
fsiddh <jasonshaw546@gmail.com>
2020-08-09 17:36:13 +0530

    updates

--- api_example/languages/serializers.py
+++ api_example/languages/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
-from .models import Language
+from .models import Language, Paradigm, Programmer
 
-class LanguageSerializer(serializers.ModelSerializer):
+class ParadigmSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Paradigm
+        fields = ('id', 'url', 'name')
+
+
+class LanguageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Language
-        fields = ('id', 'name', 'paradigm')
+        fields = ('id', 'url', 'name', 'paradigm')
+
+
+class ProgrammerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Programmer
+        fields = ('id', 'url', 'name', 'languages')
