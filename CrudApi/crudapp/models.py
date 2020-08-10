14cb040862a6bace1ecb4d0ea9357e8746f1f04d
fsiddh <jasonshaw546@gmail.com>
2020-08-10 14:49:12 +0530

    added __str__ in models

--- CrudApi/crudapp/models.py
+++ CrudApi/crudapp/models.py
@@ -5,9 +5,15 @@ from django.db import models
 class Friend(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Belonging(models.Model):
     name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
 
 
 class Borrowed(models.Model):
@@ -16,3 +22,6 @@ class Borrowed(models.Model):
     when = models.DateTimeField(auto_now_add=True)
     returned = models.DateTimeField(null=True, blank=True)
 
+    
+    def __str__(self):
+        return self.what
